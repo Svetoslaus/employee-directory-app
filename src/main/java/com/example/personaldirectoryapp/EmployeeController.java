44c6f16a3a86c9df21013f7c51b16a91f5a1b230
Svetoslaus <svetoslav.shtereff@gmail.com>
2023-06-24 01:13:54 +0200

    Add Delete Endpoint Add Get All employee

--- src/main/java/com/example/personaldirectoryapp/EmployeeController.java
+++ src/main/java/com/example/personaldirectoryapp/EmployeeController.java
@@ -6,7 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-//Anfragen bekommen
+import java.util.Optional;
+
+//Requests
 @RestController
 public class EmployeeController {
 
@@ -17,19 +19,39 @@ public class EmployeeController {
     @GetMapping("/employee")
     public ResponseEntity<Employee> get(@RequestParam(value = "id") int id){
 
-        Employee newEmployee = new Employee();
-        newEmployee.setId(10);
-        newEmployee.setDescription("Web-designer");
-        newEmployee.setIsDone(true);
+        //get emlpoyee from db by id
+        Optional<Employee>employeeInDB = employeeRepository.findById(id);
 
-        return new ResponseEntity<Employee>(newEmployee, HttpStatus.OK);
+        if(employeeInDB.isPresent()){
+            return new ResponseEntity<Employee>(employeeInDB.get(), HttpStatus.OK);
+        }
+
+        return new ResponseEntity("No Employee found by id " + id, HttpStatus.NOT_FOUND);
     }
 
+
+    @GetMapping("/employee/all")
+    public ResponseEntity<Iterable<Employee>> getAll(){
+        Iterable<Employee> allEmployeeInDB = employeeRepository.findAll();
+        return new ResponseEntity<Iterable<Employee>>(allEmployeeInDB, HttpStatus.OK);
+    }
 
     @PostMapping("/employee")
     public ResponseEntity<Employee> create(@RequestBody Employee newEmployee){
         //save employee in db
         employeeRepository.save(newEmployee);
         return new ResponseEntity<Employee>(newEmployee, HttpStatus.OK);
+    }
+
+
+    @DeleteMapping("/employee")
+    public ResponseEntity delete(@RequestParam(value = "id") int id){
+
+        Optional<Employee> employeeInDB = employeeRepository.findById(id);
+        if(employeeInDB.isPresent()){
+            employeeRepository.deleteById(id);
+            return new ResponseEntity("Employee deleted", HttpStatus.OK);
+        }
+        return new ResponseEntity("No Employee found by Id " + id, HttpStatus.NOT_FOUND);
     }
 }
